9c06a05134860e5051f2fd948da9df5e2d2adc33
Lars Melchior <TheLartians@users.noreply.github.com>
2021-10-09 17:58:25 +0200

    format code

--- test/source/matrix_tests.cpp
+++ test/source/matrix_tests.cpp
@@ -3,20 +3,20 @@
 #include <lars/matrix.h>
 using namespace lars;
 
-template <size_t size> void SquareMatrixTests(){
-  using Mat = Matrix<float, size, size> ;
-  
-  Matrix<float, size, size> matrix;
-  matrix.for_all_indices([&](DynamicIndexTuple<2> idx){
-    matrix(idx) = idx.get<0>() > idx.get<1>() ? (idx.get<0>() + 1.)/(idx.get<1>() + 1) * idx.get<1>() :
-                  (idx.get<1>() + 1.)*(idx.get<0>() + 1) + idx.get<0>();
-  });
-  
-  REQUIRE((matrix.inverse() * matrix > 1e-4) == Mat::create_identity());
+template <size_t size> void SquareMatrixTests() {
+  using Mat = Matrix<float, size, size>;
 
+  Matrix<float, size, size> matrix;
+  matrix.for_all_indices([&](DynamicIndexTuple<2> idx) {
+    matrix(idx) = idx.get<0>() > idx.get<1>()
+                      ? (idx.get<0>() + 1.) / (idx.get<1>() + 1) * idx.get<1>()
+                      : (idx.get<1>() + 1.) * (idx.get<0>() + 1) + idx.get<0>();
+  });
+
+  REQUIRE((matrix.inverse() * matrix > 1e-4) == Mat::create_identity());
 }
 
-TEST_CASE( "Matrix Tests" ) {
+TEST_CASE("Matrix Tests") {
   SquareMatrixTests<1>();
   SquareMatrixTests<2>();
   SquareMatrixTests<3>();
--- test/source/ndarray_tests.cpp
+++ test/source/ndarray_tests.cpp
@@ -5,19 +5,15 @@ using namespace lars;
 
 #include <iostream>
 
-TEST_CASE( "NDArray Tests" ) {
-  
-   using Array9 = StackNDArray<float,StaticIndexTuple<9>>;
-   
-   Array9 array;
-   array << 4,56,-34,0,5,65,-456,0.1,3.141;
-   
-   REQUIRE( array.front() == 4 );
-   REQUIRE( array.back() == 3.141f );
-   REQUIRE( array.min() == -456 );
-   REQUIRE( array.max() == 65 );
+TEST_CASE("NDArray Tests") {
 
+  using Array9 = StackNDArray<float, StaticIndexTuple<9>>;
 
+  Array9 array;
+  array << 4, 56, -34, 0, 5, 65, -456, 0.1, 3.141;
+
+  REQUIRE(array.front() == 4);
+  REQUIRE(array.back() == 3.141f);
+  REQUIRE(array.min() == -456);
+  REQUIRE(array.max() == 65);
 }
-
-
